32b425949f05d6773c433888c08ca5db4d859852
samuelamanci <samueloliveira2.ti@gmail.com>
2024-09-23 09:13:16 -0300

    Configurando Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "ozm4jh",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {
     //aqui posso colocar a baseURL
     //EX: baseUrl: 'https://example.cypress.io'
-  },
+  }, 
   video: false
 })
